2b7fa1ea86b5f473672c747813455045d70ed985
Christopher Oye <christopheroye@gmail.com>
2019-07-02 19:18:26 +0000

    added form field info to fill for courses created by instructors

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_30_213455) do
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 2019_06_30_213455) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
