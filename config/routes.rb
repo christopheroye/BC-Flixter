f7c8090e9df3fc09dfb075640a8f2b2bfa18cbd1
Christopher Oye <christopheroye@gmail.com>
2019-08-25 14:40:47 +0000

    updated code

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
-    resources :enrollments, only: [:create]
-  end
-  resources :courses, only: [:index, :show]
+    resources :enrollments, only: :create
+   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] do
-        resources :lessons, only: [:new, :create]
-      end
+    resources :lessons, only: [:update]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:new, :create]
+    end    		
+    resources :courses, only: [:new, :create, :show] do 
+      resources :sections, only: [:create]     		
     end
   end
 end
